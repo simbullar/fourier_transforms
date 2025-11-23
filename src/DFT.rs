6df92d7581fd534b9e13770f093f37fc434f804d
simbullar <simbullar@gmail.com>
2025-11-23 16:06:15 +0100

    got the DFT to work, though with some small floating point arithmetic artifacts

--- src/DFT.rs
+++ src/DFT.rs
@@ -1,18 +1,37 @@
 /* INFO DFT - my implementation of the Discrete Fourier Transform
 * the definition of the DFT is X[k] = Σ^N-1_n=0 x[n] e^(-i 2π/N kn) */
+use std::f64::consts::PI;
+
+fn e_power_i_pi_x(input: f64) -> (f64, f64) {
+    ((input * PI).cos(), (input * PI).sin())
+}
 
 // we have an array of vectors, or better said, complex numbers as our inputs and a similar array in the output
 // that means that for the input we say that it is a vector *(practically a resizable array) of a tuple of two f32s *(the complex number)
-fn dft(input: Vec<(f32, f32)>) -> Vec<(f32, f32)> {
-    let mut output: Vec<(f32, f32)> = vec![];
-    let mut i = 0;
-    for xn in input {
-        output[i] = xn; //TODO * e^(-i 2π )^(1/N kn)
-        i += 1;
+
+fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
+    let mut output: Vec<(f64, f64)> = vec![(0.0, 0.0); input.len()];
+
+    let n_big = input.len() as f64; // sample num
+
+    for k in 0..input.len() {
+        let mut sum: (f64, f64) = (0., 0.);
+        let mut n = 0;
+        for xn in &input {
+            let (cos_term, i_sin_term) = e_power_i_pi_x(-2. * k as f64 * n as f64 * 1. / n_big);
+            sum.0 += xn.0 * cos_term - xn.1 * i_sin_term;
+            sum.1 += xn.0 * i_sin_term + xn.1 * cos_term;
+
+            n += 1;
+        }
+        output[k] = sum;
     }
     output
 }
 
 fn main() {
-    dft(vec![(1., 0.), (1., 0.), (1., 0.), (1., 0.)]);
+    let result = dft(vec![(1., 0.), (1., 0.), (1., 0.), (1., 0.)]);
+    for (k, (real, imag)) in result.iter().enumerate() {
+        println!("{} + {}i", real, imag);
+    }
 }
