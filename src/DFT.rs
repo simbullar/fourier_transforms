2eed92b526976d258b570116647222f35cb31c4f
simbullar <simbullar@gmail.com>
2025-11-23 12:50:57 +0100

    removed some clutter

--- src/DFT.rs
+++ src/DFT.rs
@@ -1,5 +1,5 @@
-// DFT
-//INFO the definition of the DFT is X[k] = Σ^N-1_n=0 x[n] e^(-i 2π/N kn)
+/* INFO DFT - my implementation of the Discrete Fourier Transform
+* the definition of the DFT is X[k] = Σ^N-1_n=0 x[n] e^(-i 2π/N kn) */
 
 // we have an array of vectors, or better said, complex numbers as our inputs and a similar array in the output
 // that means that for the input we say that it is a vector *(practically a resizable array) of a tuple of two f32s *(the complex number)
@@ -7,13 +7,7 @@ fn dft(input: Vec<(f32, f32)>) -> Vec<(f32, f32)> {
     let mut output: Vec<(f32, f32)> = vec![];
     let mut i = 0;
     for xn in input {
-        output[i] = xn;
-
-        //TODO * (e^(-i 2π )^(1/N kn)
-        //FIX * (e^())
-        //FIX * e()
-        //FIX (e())
-
+        output[i] = xn; //TODO * e^(-i 2π )^(1/N kn)
         i += 1;
     }
     output
