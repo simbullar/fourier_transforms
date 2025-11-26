ee7a62a49795d32533a2e1b49043a8bbd9353919
simbullar <simbullar@gmail.com>
2025-11-26 18:22:57 +0100

    cleaned the code some more

--- src/DFT.rs
+++ src/DFT.rs
@@ -1,6 +1,6 @@
 /* INFO DFT - my implementation of the Discrete Fourier Transform
 * NOTE the definition of the DFT is X[k] = Σ^N-1_n=0 x[n] e^(-i 2π/N kn) */
-use std::{f64::consts::PI, task::RawWakerVTable};
+use std::f64::consts::PI;
 
 fn e_power_i_pi_x(input: f64) -> (f64, f64) {
     ((input * PI).cos(), (input * PI).sin())
@@ -12,10 +12,10 @@ fn e_power_i_pi_x(input: f64) -> (f64, f64) {
 fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
     let mut output: Vec<(f64, f64)> = vec![(0.0, 0.0); input.len()];
 
-    let n_big = input.len() as f64; // sample num
+    let n_big = input.len() as f64; // sample number
 
     for k in 0..input.len() {
-        let mut sum: (f64, f64) = (0., 0.);
+        let mut sum: (f64, f64) = (0., 0.); // output for this iteration
         let mut n = 0;
         for xn in &input {
             let (cos_term, i_sin_term) = e_power_i_pi_x(-2. * k as f64 * n as f64 * 1. / n_big);
@@ -24,6 +24,11 @@ fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
 
             n += 1;
         }
+
+        // INFO Removing floating-point arithmetic errors
+        sum.0 = (sum.0 * 32.).round() / 32.;
+        sum.1 = (sum.1 * 32.).round() / 32.;
+
         output[k] = sum;
     }
     output
@@ -32,14 +37,11 @@ fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
 fn main() {
     let result = dft(vec![(1., 0.), (1., 0.), (1., 0.), (1., 0.)]);
     for (k, (real, imag)) in result.iter().enumerate() {
-        // INFO fixing artifacts
-        let mut real = (real * 32.).round() / 32.;
-        let mut imag = (imag * 32.).round() / 32.;
-
-        if real == -0 as f64 {
-            real = 0.;
-        } else if imag == -0 as f64 {
-            imag = 0.;
+        if real == &(-0 as f64) {
+            let real = &0.;
+        }
+        if imag == &(-0 as f64) {
+            let imag = &0.;
         }
 
         println!("{} + i{}", real, imag);
