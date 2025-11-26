c21438e2ccf6d8337d1a5d5a69c1dd19a77f3019
simbullar <simbullar@gmail.com>
2025-11-26 19:11:21 +0100

    initialized the FFT file

---
+++ src/FFT.rs
@@ -0,0 +1,13 @@
+/* INFO  This is my derivation of the Fast Fourier Transform, made as a school project */
+
+fn fft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
+    let mut output: Vec<(f64, f64)> = vec![(0.0, 0.0); input.len()];
+
+    /* TODO actual algorithm logic */
+
+    output
+}
+
+fn main() {
+    let input = vec![(1., 0.), (1., 0.), (1., 0.), (1., 0.)];
+}
