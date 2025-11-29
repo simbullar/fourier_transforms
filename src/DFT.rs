bc97956a99d8516ee3d8cab08d454accd76415ec
simbullar <simbullar@gmail.com>
2025-11-29 12:02:19 +0100

    added the dft_2d function

--- src/DFT.rs
+++ src/DFT.rs
@@ -2,14 +2,15 @@
 * NOTE the definition of the DFT is X[k] = Σ^N-1_n=0 x[n] e^(-i 2π/N kn) */
 use std::f64::consts::PI;
 
-fn e_power_i_pi_x(input: f64) -> (f64, f64) {
+pub fn e_power_i_pi_x(input: f64) -> (f64, f64) {
+    // simple helper function, i guess
     ((input * PI).cos(), (input * PI).sin())
 }
 
 // we have an array of vectors, or better said, complex numbers as our inputs and a similar array in the output
 // that means that for the input we say that it is a vector *(practically a resizable array) of a tuple of two f32s *(the complex number)
 
-fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
+pub fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
     let mut output: Vec<(f64, f64)> = vec![(0.0, 0.0); input.len()];
 
     let n_big = input.len() as f64; // sample number
@@ -25,21 +26,44 @@ fn dft(input: Vec<(f64, f64)>) -> Vec<(f64, f64)> {
             n += 1;
         }
 
-        // INFO Removing floating-point arithmetic errors, commented out because of print and scope issues
-        /*sum.0 = (sum.0 * 32.).round() / 32.;
-        sum.1 = (sum.1 * 32.).round() / 32.; */
+        sum.0 = (sum.0 * 32.).round() / 32.;
+        sum.1 = (sum.1 * 32.).round() / 32.;
 
         output[k] = sum;
     }
     output
 }
 
+pub fn dft_2d(input: Vec<Vec<(f64, f64)>>) -> Vec<Vec<(f64, f64)>> {
+    let rows = input.len();
+    let cols = input[0].len(); // assume that all of the collumns are the same size
+
+    let mut output: Vec<Vec<(f64, f64)>> = vec![vec![(0., 0.); cols]; rows];
+
+    for y in 0..rows {
+        let row_dft = dft(input[y].clone());
+        for x in 0..cols {
+            output[y][x] = row_dft[x];
+        }
+    }
+
+    for x in 0..cols {
+        let mut column: Vec<(f64, f64)> = (0..rows).map(|y| output[y][x]).collect();
+        let cols_dft = dft(column);
+        for y in 0..rows {
+            output[x][y] = cols_dft[y];
+        }
+    }
+
+    output
+}
+
 fn main() {
-    let input = vec![(1., 0.), (1., 0.), (1., 0.), (1., 0.)];
+    let input = vec![(1., 0.), (1., 0.), (2., 0.), (1., 0.)];
     let result = dft(input);
     for (_k, (real, imag)) in result.iter().enumerate() {
-        let mut real = (real * 32.).round() / 32.;
-        let mut imag = (imag * 32.).round() / 32.;
+        let mut real = *real;
+        let mut imag = *imag;
 
         // INFO This is optional and only for output clarity
         if real == -0 as f64 {
